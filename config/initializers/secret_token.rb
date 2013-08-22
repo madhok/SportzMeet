3ae522c144a579be27c05d6516a78d2f901d32a8
Madhok Shivaratre <gsmadhok@gmail.com>
2013-08-21 22:19:07 -0700

    Creating and Editing  users

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SportzMeet::Application.config.secret_key_base = '739b4daac3888d5bf57faf7c7e48cca67ef727618cf62becbb79f7477d37608bc190d57f035dd1054e4feaba9c2a205de0cdf99dcd55078aa5ec60117707c8a8'
